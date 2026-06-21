d3d1a8bb0950152354eac3c396948e1130728f77
Jens Baumeister <jens.baumeister@gmx.net>
2026-06-21 10:46:29 +0200

    add function fetchTasks

--- scripts/board.js
+++ scripts/board.js
@@ -1,16 +1,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
-const BASE_URL = 'https://join-dca51-default-rtdb.europe-west1.firebasedatabase.app/';
+// const BASE_URL = 'https://join-dca51-default-rtdb.europe-west1.firebasedatabase.app/';
 
+let fetchedTasks
 
 function init() {
-
+    fetchTasks();
 }
 
-function openDialog(id){
+function openDialog(id) {
     document.getElementById(id).showModal();
 }
 
-function closeDialog(id){
+function closeDialog(id) {
     document.getElementById(id).close();
+}
+
+async function fetchTasks() {
+    let response;
+    let taskData;
+    try {
+        response = await fetch(BASE_URL + ".json");
+        if (!response.ok) {
+            throw new Error(`HTTP-Fehler! Status: ${response.status}`);
+        }
+        taskData = await response.json();
+        fetchedTasks = taskData
+        console.log(fetchedTasks);
+    } catch (error) {
+        console.error('Fehler beim Abrufen', error.message);
+    } finally {
+        if (!fetchedTasks || !fetchedTasks.length) {
+            return
+        }
+
+    }
 }
