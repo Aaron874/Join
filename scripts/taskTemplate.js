152deb68a409bd317b1a888077f5d027f2ddb12e
Jens Baumeister <jens.baumeister@gmx.net>
2026-06-21 17:28:11 +0200

    add function getTaskTemplate

--- scripts/taskTemplate.js
+++ scripts/taskTemplate.js
@@ -1,3 +1,8 @@
 function getTaskTemplate(task) {
-    return ``
+    const card = document.createElement('div');
+    card.classList.add('task-card');
+    const title = document.createElement('h3');
+    title.textContent = task.title;
+    card.appendChild(title);
+    return card;
 }
