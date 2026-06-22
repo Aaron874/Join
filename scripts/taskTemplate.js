02b250f02f5203bfbe3c291ae75db22c70faa85c
Jens Baumeister <jens.baumeister@gmx.net>
2026-06-22 12:05:36 +0200

    expand getTaskTemplate

--- scripts/taskTemplate.js
+++ scripts/taskTemplate.js
@@ -1,8 +1,35 @@
-function getTaskTemplate(task) {
+function getTaskTemplate(task,status) {
+
     const card = document.createElement('div');
     card.classList.add('task-card');
+
+    const category = document.createElement('span')
+    category.textContent = task.category;
+    card.appendChild(category);
+
     const title = document.createElement('h3');
     title.textContent = task.title;
     card.appendChild(title);
+
+    const dueDate = document.createElement('p');
+    dueDate.textContent = task.date;
+    card.appendChild(dueDate);
+
+    const description = document.createElement('p')
+    description.textContent = task.description;
+    card.appendChild(description);
+
+    const subTask = document.createElement('p');
+    subTask.textContent = task.subTask;
+    card.appendChild(subTask);
+
+    const collaborators = document.createElement('span');
+    collaborators.textContent = task.assignedTo;
+    card.appendChild(collaborators);
+
+    const priority = document.createElement('span');
+    priority.textContent = task.priority;
+    card.appendChild(priority);
+    
     return card;
 }
