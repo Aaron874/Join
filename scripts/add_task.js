5a473fd340c73e7b334e9e54509789e06fdc94fd
Jens Baumeister <jens.baumeister24@outlook.de>
2026-08-09 12:22:50 +0200

    add an eventListener in add_task.js for closing dropdown menus, by clicking next to them

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -30,6 +30,15 @@ const priorityConfig = {
 };
 
 window.addEventListener('DOMContentLoaded', initAddTask);
+
+document.addEventListener('click', event => {
+    if (!event.target.closest('#contacts-dropdown-wrapper'))
+        dropdownContactsUp();
+
+    if (!event.target.closest('#category-dropdown-wrapper'))
+        dropdownCategoryUp();
+});
+
 async function initAddTask() {
     try {
         await ensureAuthenticatedUser();
