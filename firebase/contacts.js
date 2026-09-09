4622d9253498d709cce261edc6420b6fd3748944
MatthiasWittl <Matthias_Wittl@outlook.de>
2026-09-09 09:23:57 +0200

    jsdoc and prettier on firebase contacts.js

--- firebase/contacts.js
+++ firebase/contacts.js
@@ -1,6 +1,10 @@
-import { getContacts }
-    from "../firebase/contact.service.js";
+import { getContacts } from '../firebase/contact.service.js';
 
+/**
+ * Loads all contacts from the backend and renders them into the UI.
+ *
+ * @returns {Promise<void>}
+ */
 async function initContacts() {
     const contacts = await getContacts();
     renderContacts(contacts);
